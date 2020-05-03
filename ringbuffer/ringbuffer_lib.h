6540f381422e83fc4a4d730fccd69c540bc947ff
coderrect <jeff@coderrect.com>
2020-05-03 02:31:32 +0000

    ringbuffer library

---
+++ ringbuffer/ringbuffer_lib.h
@@ -0,0 +1,22 @@
+//"ringbuffer_lib.h"
+#ifndef RINGBUFFER_LIB_H
+#define RINGBUFFER_LIB_H
+
+#include <cstddef>
+#include <stdexcept>
+    class RingBuffer {
+    private:
+        int *buffer_;
+        size_t write_pos_;
+        size_t available_;
+        size_t capacity_;
+
+    public:
+        RingBuffer(size_t capacity);
+        ~RingBuffer();
+
+        bool Publish(int value);
+        bool Consume(int *r);
+    };
+
+#endif //RINGBUFFER_LIB_H
